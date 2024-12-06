8c4045e3407381bad1eb61658becb53e31abb5bf
Chinna op <opchinna742@gmail.com>
2024-12-06 23:27:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
-REPO_URL = getenv("REPO_URL", "https://github.com/ITZ-ZAID/ZAID-USERBOT")
+REPO_URL = getenv("REPO_URL", "https://github.com/GDNBharat/T-series-userbot-")
 BRANCH = getenv("BRANCH", "master") #don't change
  
 STRING_SESSION1 = getenv("STRING_SESSION1", "BQGwyGIApMJ1wDWnVmlHPM5-sJbl6F4XW9CYeG-W4hAF0KccqJvm3DaFc0NGM9y0xVsJ4fpG8qbJAqHxxT7uIRZdepMr0QxL_sMQjCMSoXIRm4YBNX5376lnJ4pdvDVXSt26vDP11DA6rhQzfM7MCDbYSc1vMQM2wOfqIIHdvwYXHe90MCmbHLZh9TRh4U7QktiZa6JX2SfYIfQCHYcGBO1gxtGzMzQdnJ4539w6IZALkomhfQFw6mTa6tJ70D_3XVSmCmOBgbir-fVA3ha63tC6jwvJ2cNpP3P-iaSqVB9aKA6B2UBUXGYvTJyaH8NJiVGeqHpZ80qH7d2ShMll2Izqf1WGxQAAAAHETnMdAA")
